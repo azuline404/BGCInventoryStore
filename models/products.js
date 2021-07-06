4255a6bce16bd9c087ab953969212149bbfd1e87
abielk <abielk@sfu.ca>
2021-07-05 20:49:43 -0700

    update products.js

--- models/products.js
+++ models/products.js
@@ -1,53 +1,38 @@
+const pool = require("../util/postgresql");
 let pg = ('../util/postgresql');
 
-const getAll = () => {
-    return new Promise((resolve, reject) => {
-        pg.query(
-            `SELECT * from products`)
-            .then((res, err) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(res.rows);
-        });
-    });
-};
 
-const getProduct = (productID) => {
-    return new Promise((resolve, reject) => {
-        pg.query(
-            `SELECT * from products products.id = ` + id)
-            .then((res, err) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(res.rows);
-        });
-    });
-};
+async function getAllProducts()
+{
+    try {
+        const res = await pg.query(`SELECT * FROM products`)
+        return res.rows
+    } catch (err) {
+        return err.stack
+    }
+}
 
-const getCategory = (category) => {
-    return new Promise((resolve, reject) => {
-        pg.query(
-            `SELECT * from products where category =  ` + category)
-            .then((res, err) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(res.rows);
-        });
-    });
-};
+async function getProductsByCategory(productCategory)
+{
+    try {
+        const res = await pg.query(`SELECT * FROM products WHERE category = ${productCategory}`);
+        return res.rows
+    } catch (err) {
+        return err.stack
+    }
+}
 
-const getSpecificProduct = (productID, productDescriptionID) => {
-    return new Promise((resolve, reject) => {
-        pg.query(
-            `SELECT * from products INNER JOIN where products.id = ` + id + `AND productDescriptionID =` + productDescriptionID)
-            .then((res, err) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(res.rows);
-        });
-    });
-};
+async function getProductByID(productID)
+{
+    try {
+        const res = await pg.query(`SELECT * FROM products WHERE productID = ${productID}`)
+        return res.rows
+    } catch(err) {
+        return err.stack
+    }
+}
+
+// INVOCATION:
+// var allProducts = await getAllProducts()
+// var allProductsInCategory = await getProductsByCategory('bottles')
+// var specificProduct = await getProductByID()
