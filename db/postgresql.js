85a7da5c0a09af63509c88ee5046d7a33fa2ab17
mac <2664454673@qq.com>
2021-08-03 14:21:58 +0800

    format issue

--- db/postgresql.js
+++ db/postgresql.js
@@ -2,22 +2,22 @@
 // abiel's password ==> root
 // wenqing's password ==> ba13641228671
 
-const { Pool } = require('pg')
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'lol123',
-  port: 5432,
-})
-module.exports = pool;
-
 // const { Pool } = require('pg')
 // const pool = new Pool({
 //   user: 'postgres',
 //   host: 'localhost',
 //   database: 'postgres',
-//   password: 'root',
+//   password: 'lol123',
 //   port: 5432,
 // })
 // module.exports = pool;
+
+const { Pool } = require('pg')
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'iteration2',
+  password: 'ljc0000312Li',
+  port: 5432,
+})
+module.exports = pool;
