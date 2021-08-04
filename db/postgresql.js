48bf6664c9255dc9e4397c66a33c82736a764120
mac <2664454673@qq.com>
2021-08-04 10:13:48 +0800

    push richard

--- db/postgresql.js
+++ db/postgresql.js
@@ -16,8 +16,8 @@ const { Pool } = require('pg')
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'postgres',
-  password: 'lol123',
+  database: 'iteration2',
+  password: 'ljc0000312Li',
   port: 5432,
 })
 module.exports = pool;
