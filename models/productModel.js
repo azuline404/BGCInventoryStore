08df5eb7eac5c67da5c70ec1716c74b83eccac8f
wenqing liu <wenqingl@sfu.ca>
2021-08-03 19:09:48 -0700

    .

--- models/productModel.js
+++ models/productModel.js
@@ -77,6 +77,27 @@ const productModelControls = {
         return await queryDB(`SELECT * FROM products INNER JOIN product_details ON products.product_id = product_details.product_id WHERE products.product_id =` + n)
     },*/
 
+    async insertProduct(name,description,value,category){
+        return await queryDB(`INSERT INTO products (product_name, product_desc, value, category) VALUES ('${name}', '${description}', '${value}', '${category}') RETURNING product_id`)
+    },
+
+    async insertProductDetails(sku_id, product_id, size, gender, color, imgurl){
+        return await queryDB(`INSERT INTO product_details (sku_id, product_id, size, gender, color, product_img) VALUES ('${sku_id}', '${product_id}', '${size}', '${gender}', '${color}', '${imgurl}')`)
+    },
+    async insertEmptyProductCount(sku_id){
+        await queryDB(`INSERT INTO product_details_offices (sku_id, location, quantity) VALUES ('${sku_id}', 'Burnaby', '0')`)
+        await queryDB(`INSERT INTO product_details_offices (sku_id, location, quantity) VALUES ('${sku_id}', 'Metrotown', '0')`)
+        await queryDB(`INSERT INTO product_details_offices (sku_id, location, quantity) VALUES ('${sku_id}', 'New Westminster', '0')`)
+        await queryDB(`INSERT INTO product_details_offices (sku_id, location, quantity) VALUES ('${sku_id}', 'Richmond', '0')`)
+        await queryDB(`INSERT INTO product_details_offices (sku_id, location, quantity) VALUES ('${sku_id}', 'Surrey', '0')`)
+        await queryDB(`INSERT INTO product_details_offices (sku_id, location, quantity) VALUES ('${sku_id}', 'Vancouver', '0')`)
+    },  
+    async getAllProducts(){
+        return await queryDB(`SELECT * FROM product_details INNER JOIN products ON product_details.product_id = products.product_id`)
+    },
+    async getAllProductCounts(){
+        return await queryDB(`SELECT * FROM product_details_offices`)
+    },
     async getOneProduct(m){
         return await queryDB(`SELECT * FROM products WHERE product_id = ` + m)
     },
@@ -96,7 +117,21 @@ const productModelControls = {
     async getAllImage(m){
         return await queryDB(`SELECT DISTINCT product_img FROM product_details WHERE product_id = ` + m)
     },
-
+    async updateProductTable(productID, newName, newDesc, newValue, newCategory){
+        return await queryDB(`UPDATE products set product_name = '${newName}', product_desc = '${newDesc}', value = '${newValue}', category = '${newCategory}' WHERE product_id = '${productID}'`)
+    },
+    async updateProductDetailsTable(skuID, newGender, newSize, newColor, newImage){
+        return await queryDB(`UPDATE product_details set gender = '${newGender}', size = '${newSize}', color = '${newColor}', product_img = '${newImage}' 
+        WHERE sku_id = '${skuID}'`);
+    },
+    async updateProductDetailsOfficesTable(skuID, newBurnabyQty, newMetroQty, newNewWestQty, newRichmondQty, newSurreyQty, newVancouverQty) {
+        await queryDB(`UPDATE product_details_offices SET quantity = '${newBurnabyQty}' WHERE location = 'Burnaby' and sku_id = '${skuID}'`)
+        await queryDB(`UPDATE product_details_offices SET quantity = '${newMetroQty}' WHERE location = 'Metrotown' and sku_id = '${skuID}'`)
+        await queryDB(`UPDATE product_details_offices SET quantity = '${newNewWestQty}' WHERE location = 'New Westminster' and sku_id = '${skuID}'`)
+        await queryDB(`UPDATE product_details_offices SET quantity = '${newRichmondQty}' WHERE location = 'Richmond' and sku_id = '${skuID}'`)
+        await queryDB(`UPDATE product_details_offices SET quantity = '${newSurreyQty}' WHERE location = 'Surrey' and sku_id = '${skuID}'`)
+        await queryDB(`UPDATE product_details_offices SET quantity = '${newVancouverQty}' WHERE location = 'Vancouver' and sku_id = '${skuID}'`)
+    },
 }
 
 module.exports = productModelControls;
