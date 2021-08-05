bc02ef5219d14aad278a3d1567e88566e2019e75
wenqing liu <wenqingl@sfu.ca>
2021-08-04 17:21:29 -0700

    image rotation

--- db/postgresql.js
+++ db/postgresql.js
@@ -1,12 +1,22 @@
 // tommy's password ==> lol123
 // abiel's password ==> root
 // wenqing's password ==> ba13641228671
-const { Pool } = require('pg')
+
+/*const { Pool } = require('pg')
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'postgres',
   password: 'lol123',
+  port: 5432,
+})*/
+
+const { Pool } = require('pg')
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'bgc2',
+  password: 'ba13641228671',
   port: 5432,
 })
 
